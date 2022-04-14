e74fd774db7de0671f28a832bb267e0d4d240b44
Rawthul <ozgurgokd@gmail.com>
2022-04-14 06:21:43 +0300

    Add Country Flags for Language Menu

--- src/components/LanguageMenu/LanguageMenu.js
+++ src/components/LanguageMenu/LanguageMenu.js
@@ -1,32 +1,52 @@
 import { useContext } from 'react';
 import { LocaleContext } from '../../store/locale-context';
-import { Menu, MenuButton, IconButton , MenuList, MenuItem } from '@chakra-ui/react';
+import {
+	Menu,
+	MenuButton,
+	IconButton,
+	MenuList,
+	MenuItem,
+} from '@chakra-ui/react';
 import { IoLanguage } from 'react-icons/io5';
+import Flags from 'country-flag-icons/react/3x2';
 
 const LanguageMenu = () => {
 	const { language, setLanguage } = useContext(LocaleContext);
 
-	const localeCodes = { English: 'en', Türkçe: 'tr' };
-
-	const handleClick = (language) => setLanguage(language);
+	const languages = [
+		{
+			name: 'English',
+			code: 'en',
+			country: 'GB',
+		},
+		{
+			name: 'Türkçe',
+			code: 'tr',
+			country: 'TR',
+		},
+	];
 
 	return (
-		<Menu autoSelect={false} gutter='12'>
+		<Menu autoSelect={false} gutter='12' placement='auto'>
 			<MenuButton as={IconButton} icon={<IoLanguage />} isRound={true} />
 			<MenuList minWidth='fit-content'>
-				{Object.keys(localeCodes).map((key) => (
-					<MenuItem
-						key={key}
-						isDisabled={language === localeCodes[key]}
-            fontWeight='medium'
-						onClick={handleClick.bind(null, localeCodes[key])}
-					>
-						{key}
-					</MenuItem>
-				))}
+				{languages.map(({ name, code, country }) => {
+					const Flag = Flags[country];
+					return (
+						<MenuItem
+							key={country}
+							icon={<Flag width={24} />}
+							isDisabled={language === code}
+							fontWeight='medium'
+							onClick={setLanguage.bind(null, code)}
+						>
+							{name}
+						</MenuItem>
+					);
+				})}
 			</MenuList>
 		</Menu>
 	);
-}
+};
 
 export default LanguageMenu;
