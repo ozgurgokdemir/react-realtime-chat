443080fe6bdbe01f9ccba7142b6a9ced7c9c6e7e
Rawthul <ozgurgokd@gmail.com>
2022-04-21 23:36:51 +0300

    Add Initial Loading of User Chats

--- src/pages/Messages/Messages.js
+++ src/pages/Messages/Messages.js
@@ -1,27 +1,43 @@
-import {  useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import { loadDocs } from '../../services/firebase/firestore';
+import { loadDocs, getDoc, getDocs } from '../../services/firebase/firestore';
 import { useAuth } from '../../store/auth-context';
 
 const Messages = () => {
-  const [chats, setChats] = useState([]);
-  const { user } = useAuth();
+	const [chats, setChats] = useState([]);
+	const { user } = useAuth();
 
 	useEffect(() => {
-    loadDocs(`users/${user.uid}/chats`, 'desc', 10, (snapshot) => {
-      const data = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-      console.log(data)
-      setChats(data);
-    });
-  }, [user.uid]);
+		const fetchChatMemberIds = async (chatId) => {
+			const querySnapshot = await getDocs(`chats/${chatId}/members`);
+			return querySnapshot.docs.map(({ id }) => id);
+		};
+		const fetchUserData = async (userId) => {
+			const querySnapshot = await getDoc('users', userId);
+			return querySnapshot.data();
+		};
+		const handleSnapshot = (snapshot) => {
+			const chatIds = snapshot.docs.map(({ id }) => id);
+			chatIds.forEach(async (chatId) => {
+				const memberIds = await fetchChatMemberIds(chatId);
+				const contactId = memberIds.find((memberId) => memberId !== user.uid);
+				const contactInfo = await fetchUserData(contactId);
+				const contact = { uid: contactId, name: contactInfo.name };
+				setChats([{ id: chatId, contact }]);
+			});
+		};
+		loadDocs(`users/${user.uid}/chats`, null, handleSnapshot);
+	}, [user.uid]);
 
 	return (
-    <div className='w-full flex flex-col'>
-      {chats.map(({id}) => (
-        <Link to={id} key={id} className='px-4 py-6'>{id}</Link>
-      ))}
-    </div>
+		<div className='w-full flex flex-col'>
+			{chats.map(({ id, contact }) => (
+				<Link to={id} key={id} className='px-4 py-6'>
+					{contact.name}
+				</Link>
+			))}
+		</div>
 	);
 };
 
--- src/services/firebase/firestore.js
+++ src/services/firebase/firestore.js
@@ -1,13 +1,15 @@
 import {
 	getFirestore,
 	collection,
+	doc as document,
 	query,
 	onSnapshot,
 	serverTimestamp,
 	orderBy,
 	limit,
 	addDoc as add,
-	getDocs as get,
+	getDocs as getMultiple,
+	getDoc as getSingle,
 } from 'firebase/firestore';
 
 import { app } from './app';
@@ -17,13 +19,15 @@ export const db = getFirestore(app);
 export const addDoc = (path, data) =>
 	add(collection(db, path), { ...data, timestamp: serverTimestamp() });
 
-export const getDocs = (path) => get(collection(db, path));
+export const getDocs = (path) => getMultiple(collection(db, path));
 
-export const loadDocs = (path, direction, limitNum, callback) => {
-	const recentDocsQuery = query(
-		collection(db, path),
-		orderBy('timestamp', direction),
-		limit(limitNum)
-	);
+export const getDoc = (coll, doc) => getSingle(document(db, coll, doc));
+
+export const loadDocs = (path, cstr, callback) => {
+	const docData = collection(db, path);
+	const orderQuery = cstr?.field ? orderBy(cstr.field, cstr.direction) : null;
+	const limitQuery = cstr?.limit ? limit(cstr.limit) : null;
+	const constraints = [orderQuery, limitQuery].filter(Boolean);
+	const recentDocsQuery = query(docData, ...constraints);
 	onSnapshot(recentDocsQuery, callback);
 };
