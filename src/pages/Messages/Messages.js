5142342b31581665fcf71e0d977b9f80302b6a26
Rawthul <ozgurgokd@gmail.com>
2022-05-10 20:53:10 +0300

    Update Messages Design

--- src/pages/Messages/Messages.js
+++ src/pages/Messages/Messages.js
@@ -1,11 +1,9 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import {
-	listenDocument,
-	getDocument,
-	getCollection,
-} from '../../services/firebase/firestore';
+import { listenDocument, getDocument } from '../../services/firebase/firestore';
 import { useAuth } from '../../store/auth-context';
+
+import { Avatar } from '@chakra-ui/react';
 
 const Messages = () => {
 	const [chats, setChats] = useState([]);
@@ -13,9 +11,9 @@ const Messages = () => {
 	const { user } = useAuth();
 
 	useEffect(() => {
-		const fetchChatMemberIds = async (chatId) => {
-			const querySnapshot = await getCollection(`chats/${chatId}/members`);
-			return querySnapshot.docs.map(({ id }) => id);
+		const fetchChatMembers = async (chatId) => {
+			const querySnapshot = await getDocument('chats', chatId);
+			return querySnapshot.data().members;
 		};
 		const fetchUserData = async (userId) => {
 			const querySnapshot = await getDocument('users', userId);
@@ -25,8 +23,8 @@ const Messages = () => {
 			if (!chats) return [];
 			return Promise.all(
 				chats.map(async (chatId) => {
-					const memberIds = await fetchChatMemberIds(chatId);
-					const contactId = memberIds.find((memberId) => memberId !== user.uid);
+					const memberIds = await fetchChatMembers(chatId);
+					const contactId = memberIds.find((id) => id !== user.uid);
 					const contactInfo = await fetchUserData(contactId);
 					const contact = {
 						uid: contactId,
@@ -49,10 +47,11 @@ const Messages = () => {
 	const chatsContent = (
 		<ul className='flex flex-col'>
 			{chats.map(({ id, contact }) => (
-				<li key={id} className='inline-block'>
-					<Link to={id} className='inline-block w-full px-4 py-6'>
-						{contact.displayName}
-					</Link>
+				<li key={id} className='border-b border-[color:#ffffff0f] dark:border-[color:#000000f0]'>
+					<Link to={id} className='flex gap-4 items-center p-4'>
+					  <Avatar name={contact.displayName} src={contact.photoURL} />
+            <span className='font-medium text-lg'>{contact.displayName}</span>
+          </Link>
 				</li>
 			))}
 		</ul>
